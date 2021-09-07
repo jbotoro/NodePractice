5d0c79b60faeb10e1c229d10c9bd3030c9c4b123
Jordan Black <jblack530@gmail.com>
2021-09-07 12:00:20 -0700

    started header / nav / results components: Header + HeaderItem Basic tailwindcss setup and heroicons

---
+++ NextandReact/hulu-2-practice/components/Header.js
@@ -0,0 +1,30 @@
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon
+} from "@heroicons/react/outline"
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+
+
+function Header() {
+    return (
+        <header className=''>
+            <div>
+                <HeaderItem title='HOME' Icon={HomeIcon} />
+            </div>
+            <Image 
+                alt = "Hulu Image"
+                className="object-contain"
+                src = 'https://links.papareact.com/ua6'
+                width = {200}
+                height = {100}
+            />
+        </header>
+    )
+}
+
+export default Header
---
+++ NextandReact/hulu-2-practice/components/HeaderItem.js
@@ -0,0 +1,10 @@
+function HeaderItem({ Icon, title }) {
+    return (
+        <div className="flex flex-col items-center group">
+            <Icon  className="h-8 mb-1"/>
+            <p className="opacity-0 group-hover:opacity-100 tracking-widest"> {title} </p>
+        </div>
+    )
+}
+
+export default HeaderItem
