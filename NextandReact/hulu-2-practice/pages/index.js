25dc7d336a9987319a2e4a7885c4c7c6c46c3b0b
Jordan Black <jblack530@gmail.com>
2021-09-08 12:23:26 -0700

    changed casing on header import index.js

--- NextandReact/hulu-2-practice/pages/index.js
+++ NextandReact/hulu-2-practice/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Header from '../components/header'
+import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
 import requests from '../utils/requests'
