13d55140a4c369129bb56a927e2a6710fc636d46
Jordan Black <jblack530@gmail.com>
2021-09-08 12:08:42 -0700

    finished up last touches of styling for results component, done with demon build next steps for the future to add functionality to the header buttons / user info

--- NextandReact/hulu-2-practice/components/Thumbnail.js
+++ NextandReact/hulu-2-practice/components/Thumbnail.js
@@ -1,11 +1,13 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image"
+import { forwardRef } from "react";
 
-function Thumbnail({ result }) {
+// eslint-disable-next-line react/display-name
+const Thumbnail = forwardRef( ({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
     return (
-        <div className='p-2 group cursor-pointer transition duration-200 
+        <div ref={ref} className='p-2 group cursor-pointer transition duration-200 
         ease-in transform sm:hover:scale-105 hover:z-50' >
             <Image
                 layout='responsive'
@@ -33,6 +35,6 @@ function Thumbnail({ result }) {
             </div>
         </div>
     )
-}
+})
 
 export default Thumbnail
